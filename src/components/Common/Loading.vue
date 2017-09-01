978e2156f8183eef318b3b5623c10ed3143e196b
xLong <381612175@qq.com>
2017-09-01 13:44:02 +0800

    新增：Loading.vue

---
+++ src/components/Common/Loading.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="loading" class="page-load">
+        <Spin>
+            <Icon type="load-c" size=18 class="spin-icon-load"></Icon>
+            <div>玩命加载中...</div>
+        </Spin>
+    </div>
+</template>
+<script>
+  export default {
+    name: 'loading',
+    data () {
+      return {}
+    }
+  }
+</script>
+<style lang="less" scoped>
+  .page-load{
+    margin-top: 18%;
+  }
+</style>
+
