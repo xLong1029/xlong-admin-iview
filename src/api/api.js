bc791e35a7d319c8644e02a6df610f3a47d821e2
xLong <381612175@qq.com>
2017-09-04 13:11:53 +0800

    新增：修改资料api

--- src/api/api.js
+++ src/api/api.js
@@ -9,28 +9,31 @@ import { GetParams } from 'common/important.js'
 
 export default {
     // 登录
+    // params: 修改的参数对象
     Login : (params) => {
         let query = BmobServer.Query('Login');
         // 两条查询语句一起写，就相当于AND查询
         query.equalTo('username', params.username);
         query.equalTo('password', params.password);
         return new Promise((resolve, reject) => {
+            // 仅获取一行数据
 			query.first({
                 success: (res) => resolve({ code: 200, data: res }),
                 err: (err) => reject({ code: 0, data: err })
             });
-		})
+		});
     },
     // 获取用户信息
-    GetUserInfo: (params) => {
+    // token: 用户token参数
+    GetUserInfo: (token) => {
         let query = BmobServer.Query('UserInfo');
-        query.equalTo('token', params.token);
+        query.equalTo('token', token);
         return new Promise((resolve, reject) => {
 			query.first({
                 success: (res) => resolve({ code: 200, data: res }),
                 err: (err) => reject({ code: 0, data: err })
             });
-		})
+		});
     },
     // 修改个人资料
     // params: 修改的参数对象, key：查询的唯一键
@@ -40,7 +43,6 @@ export default {
         let p = GetParams(key);
         // 根据唯一键查询对象
         query.equalTo(p.key[0], p.value[0]);
-        // 仅获取一行数据
         return new Promise((resolve, reject) => { 
             query.first({
                 success: (obj) => {
@@ -55,8 +57,8 @@ export default {
                     });                    
                 },
                 error: (err) => console.log('无法通过该键值对获取数据')
-            })
-        })
+            });
+        });
     },
     // 修改密码
     // params: 修改的参数对象, key：查询的唯一键
@@ -66,9 +68,8 @@ export default {
         let p = GetParams(key);
         // 根据唯一键查询对象
         query.equalTo(p.key[0], p.value[0]);
-        query.equalTo('password', params.oldPassword);
-        // 仅获取一行数据
-        return new Promise((resolve, reject) => { 
+        query.equalTo('password', params.oldPassword);        
+        return new Promise((resolve, reject) => {
             query.first({
                 success: (obj) => {
                     if(obj == undefined){
@@ -83,12 +84,12 @@ export default {
                     });                    
                 },
                 error: (err) => console.log('无法通过该键值对获取数据')
-            })
-        })
+            });
+        });
     },
     // 获取账户列表
     // num：返回的条数
-    GetAccountList: (num) => {
+    GetAccList: (num) => {
         let query = BmobServer.Query('Account');
         // 默认返回10条数据
         query.limit(num);
@@ -96,7 +97,52 @@ export default {
             query.find({
                 success: (res) => resolve({ code: 200, data: res }),
                 err: (err) => reject({ code: 0, data: err })
-            })
-        })
+            });
+        });
+    },
+    // 新增账户
+    // params: 修改的参数对象
+    AddAccount: (params) => {
+        let obj = BmobServer.Add('Account');
+        return new Promise((resolve, reject) => { 
+            // 添加数据，第一个入口参数是Json数据
+            obj.save(params, {
+                success: (res) => resolve({ code: 200, data: res }),
+                error: (res, err) => reject({ code: 0, data: err })
+            });
+        });
+    },
+    // 获取账户信息
+    // id：查询的objectId
+    GetAccInfo: (id) => {
+        let query = BmobServer.Query('Account');
+        return new Promise((resolve, reject) => { 
+            //查询单条数据，第一个参数是这条数据的objectId值
+            query.get(id, {
+                success: (res) => resolve({ code: 200, data: res }),
+                error: (res, err) => reject({ code: 0, data: err })
+            });
+        });
+    },
+    // 修改账户信息
+    // params: 修改的参数对象, id：查询的objectId
+    EditAccount: (params, id) => {
+        let query = BmobServer.Query('Account');
+        return new Promise((resolve, reject) => {
+            query.get(id, {
+                success: (obj) => {
+                    if(obj == undefined){
+                        resolve({ code: 404, msg: '无该id用户数据可获取！' });
+                        return false;
+                    }
+                    // 设置并保存数据
+                    obj.save(params, {
+                        success: (res) => resolve({ code: 200, data: res }),
+                        err: (err) => reject({ code: 0, data: err })
+                    });                    
+                },
+                error: (err) => console.log(err)
+            });
+        });
     },
 }
