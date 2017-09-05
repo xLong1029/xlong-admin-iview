35728f096f0921c31f39d4217a3252efc62b2b0b
xLong <381612175@qq.com>
2017-09-05 18:03:43 +0800

    新增：二维码图片

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -5,11 +5,20 @@
         <p>组件页面里的相关组件，是本人由于工作需要根据Iview原有组件进行部分重写的新组件。</p>
         <br/>
         <p>示例页面里的相关页面，是本人整理出来曾用过一些功能页面。</p>
+        <br/>
+        <br/>
+        <img :src="qrcode"/>
+        <p style="margin-left:50px">扫一扫，关注xLong设计</p>
     </div>
 </template>
 
 <script>
     export default {
+        data(){
+            return{
+                qrcode: require('assets/images/qrcode.jpg')
+            }
+        },
         created(){
             // 设置面包屑路径
             this.$store.commit('SET_BREADCRUMB', [{ name: '首页', path: '/Home' }]);
