36cb3cba1825a4ec796f1b41e047ae4d2d3b0873
xLong <381612175@qq.com>
2017-09-05 16:22:29 +0800

    新增：查询列表和获取所有列表方法

--- src/mixins/table_query.js
+++ src/mixins/table_query.js
@@ -34,10 +34,11 @@ export default {
 		// 获取查询起始和结束时间
 		getDate(date) {
 			console.log('get date:' + date);
-			this.queryForm.createTimeStart = date[0];
-			this.queryForm.createTimeEnd = date[1];
+			this.queryForm.sTime = date[0] + ' 00:00:00';
+			this.queryForm.eTime = date[1] + ' 00:00:00';
 		},
-		// 查询 form:表单对象， getValid:是否需要验证表单
+		// 查询
+		// form:表单对象， getValid:是否需要验证表单
 		query(form, getValid) {
 			// 修改当前页码
 			this.page.pageNo = 1;
@@ -51,6 +52,52 @@ export default {
 				})
 			}
 			else this.getTableList(true);
+		},
+		// 获取筛选列表
+		getFilterList(){
+			this.apiGetFilter()
+			.then(res => {
+			    this.pageLoading = false;
+			    if(res.code == 200){
+					/* 筛选制定日期内的数据 */
+					let result = [], page;					
+					if(this.queryForm.sTime != '' && this.queryForm.eTime != ''){
+						for(let item of res.data){
+							// 比较日期大小，若第一个值小于第二个值则返回true
+							let start = Common.CompareDate(this.queryForm.sTime, item.createdAt),
+								end = Common.CompareDate(item.createdAt, this.queryForm.eTime);
+							// 满足条件则保留数据
+							if(start && end){
+								result.push(item);
+							}
+						}
+						// 页码
+                    	page = { count: result.length, pages: Math.ceil(result.length / pageSize) };
+					}
+					/* 筛选制定日期内的数据 */
+					
+			        // 设置数据
+					this.setListData(result);
+					// 设置页码
+					this.setPage(page);
+			    }
+			    else this.$Message.error('获取数据失败!');
+			})
+			.catch(err => console.log(err))
+		},
+		// 获取所有列表
+		getAllList(){
+			this.apiGetAll().then(res => {
+				this.pageLoading = false;
+				if(res.code == 200){
+					// 设置数据
+					this.setListData(res.data);
+					// 设置页码
+					this.setPage(res.page);
+				}
+				else this.$Message.error('获取数据失败!');
+			})
+			.catch(err => console.log(err))
 		},
 	}
 }
