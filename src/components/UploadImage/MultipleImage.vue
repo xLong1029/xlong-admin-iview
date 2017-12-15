02899d52ba7e8b4b72f95d12bbe3ff2b02a364d7
xLong <381612175@qq.com>
2017-12-15 11:00:25 +0800

    修改：图片上传样式，图片在显示框中居中显示

--- src/components/UploadImage/MultipleImage.vue
+++ src/components/UploadImage/MultipleImage.vue
@@ -2,7 +2,7 @@
     <div id='multipleImage' class="m-Upload">
         <!-- 图片显示 -->
         <div :ref="'upload-img-'+index" v-for="(item, index) in getImageUrlArr" :key="index" class="upload-show">
-            <img class="upload-image" :src="item" alt="图片加载失败" @error="notFoundPic"/>
+            <div class="upload-img-wrap"><img class="upload-image" :src="item" alt="图片加载失败" @error="notFoundPic"/></div>
             <!-- 可预览遮罩 -->
             <div class="upload-mask">
                 <Icon v-if="preview" type="eye" style="margin-right:15px;" @click.native="viewImage(item)"></Icon>
