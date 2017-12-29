804c7cfb44b4ba3fab1b99371d307ab67be09bc2
xLong <381612175@qq.com>
2017-12-29 16:18:49 +0800

    新增：联系信息

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -9,6 +9,16 @@
         <br/>
         <img :src="qrcode"/>
         <p style="margin-left:50px">扫一扫，关注xLong设计</p>
+        <br/>
+        <br/>
+        <p>联系方式（一个不务正业的前端工程师）</p>
+        <br/>
+        <p>QQ：381612175</p>
+        <p>github：<a href="https://github.com/xLong1029/" target="_blank">https://github.com/xLong1029/</a></p>
+        <p>站酷主页：<a href="http://xlong.zcool.com.cn/" target="_blank">http://xlong.zcool.com.cn/</a></p>
+        <p>UI中国：<a href="http://i.ui.cn/ucenter/358591.html" target="_blank">http://i.ui.cn/ucenter/358591.html</a></p>
+        <br/>
+        <p>*此项目做学习Vuejs测试使用，有什么问题可联系我讨论</p>
     </div>
 </template>
 
