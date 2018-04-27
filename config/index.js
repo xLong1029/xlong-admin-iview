de3e3ca9ca0e990fa5a0f288d7badc860623c1bc
xLong <381612175@qq.com>
2018-04-27 17:07:30 +0800

    修改webpack配置文件

--- config/index.js
+++ config/index.js
@@ -7,7 +7,8 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    // 将"/"修改成"./"解决打包后资源文件报错404的问题
+    assetsPublicPath: './',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
