380e95ca4fd72793b1b623c9a196358460c99a61
xLong <381612175@qq.com>
2019-09-28 14:25:51 +0800

    优化

--- src/views/Error/Err404.vue
+++ src/views/Error/Err404.vue
@@ -2,6 +2,7 @@
   <div class="error-content">
     <img :src="require('@/assets/images/404.jpg')"/>
     <div>
+      <Button type="primary" class="g-mr10" @click="$router.push({ name: 'Home'})">首页</Button>
       <Button type="primary" @click="$router.go(-1)">返回上一页</Button>
     </div>
   </div>
