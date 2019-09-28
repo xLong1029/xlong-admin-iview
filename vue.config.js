298af9187af233157500797008d3874655595f11
xLong <381612175@qq.com>
2019-09-28 16:05:21 +0800

    优化

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ function resolve(dir) {
 module.exports = {
     // 部署应用程序的基本URL
     // 默认情况下，将部署在域的根目录下。如果部署在子路径上，则需要使用此选项指定子路径。例如，部署在https://www.foobar.com/my-app/，则publicPath到'/my-app/'
-    publicPath: production ? '/xlong-admin-vue/' : '',
+    publicPath: production ? '/xlong-admin-vue/' : '/',
     // 构建输出目录，在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）
     outputDir: 'dist',
     // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
