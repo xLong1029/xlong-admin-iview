7f3a722fd6c4446a1c16ea302de216c9b6a23a00
xLong <381612175@qq.com>
2019-09-29 10:45:12 +0800

    优化

--- src/views/Layout/Main.vue
+++ src/views/Layout/Main.vue
@@ -32,60 +32,6 @@ export default {
   },
   data() {
     return {
-      // menuList: [
-      //   {
-      //       name: 'Home',
-      //       icon: 'md-home',
-      //       text: 'XLONG家里蹲管理系统',
-      //       isTitle: true
-      //   },
-      //   {
-      //     name: "Componets",
-      //     icon: "ios-keypad",
-      //     text: "自定义组件",
-      //     submenu: [
-      //       {
-      //         name: "Image",
-      //         text: "Image图片操作"
-      //       },
-      //       {
-      //         name: "Input",
-      //         text: "Input输入框"
-      //       },
-      //       {
-      //         name: "Sidebar",
-      //         text: "侧边栏"
-      //       }
-      //     ]
-      //   },
-      //   {
-      //     name: "Examples",
-      //     icon: "ios-paper",
-      //     text: "布局示例",
-      //     submenu: [
-      //       {
-      //         name: "AccountList",
-      //         text: "账户列表"
-      //       },
-      //       {
-      //         name: "SectionList",
-      //         text: "版块列表"
-      //       },
-      //       {
-      //         name: "ProductList",
-      //         text: "产品列表"
-      //       },
-      //       {
-      //         name: "ArticleList",
-      //         text: "文章列表"
-      //       },
-      //       {
-      //         name: "MessageList",
-      //         text: "消息列表"
-      //       }
-      //     ]
-      //   }
-      // ]
     };
   }
 };
